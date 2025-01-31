abbf257287b8e8dd8e30869365f94f891b926d8b
Hoang D. Nguyen <hdn.account@icloud.com>
2025-01-31 21:28:35 +1000

    Remove closures and put it into functions for FileManagerClient

--- photo-copy/Services/FileManagerClient.swift
+++ photo-copy/Services/FileManagerClient.swift
@@ -2,71 +2,66 @@ import ComposableArchitecture
 import Foundation
 
 struct FileManagerClient {
-  var createDirectory: @Sendable (URL, String) async -> Result<URL, FileCopyService.FileCopyError>
-  var getDirectory: @Sendable (URL, String) async -> Result<URL, FileCopyService.FileCopyError>
-  var listContents: @Sendable (URL) async -> Result<[URL], FileCopyService.FileCopyError>
-  var secureBaseFolder: @Sendable (URL) async -> Result<URL, FileCopyService.FileCopyError>
-  var countPhotos: @Sendable (URL) async -> Result<Int, FileCopyService.FileCopyError>
+  func createDirectory(_ url: URL, _ name: String) async -> Result<URL, FileCopyService.FileCopyError> {
+    SecurityScopedHelper.access(url) {
+      let newDirURL = url.appendingPathComponent(name)
+      do {
+        try FileManager.default.createDirectory(
+          at: newDirURL,
+          withIntermediateDirectories: true,
+          attributes: nil
+        )
+        return SecurityScopedHelper.createSecureBookmark(for: newDirURL)
+      } catch {
+        return .failure(.customerDirectoryCreationFailed)
+      }
+    }
+  }
+  
+  func getDirectory(_ baseURL: URL, _ name: String) async -> Result<URL, FileCopyService.FileCopyError> {
+    let dirURL = baseURL.appendingPathComponent(name)
+    if FileManager.default.fileExists(atPath: dirURL.path) {
+      return SecurityScopedHelper.createSecureBookmark(for: dirURL)
+    } else {
+      return .failure(.directoryNotFound(dirURL.lastPathComponent))
+    }
+  }
+  
+  func listContents(_ url: URL) async -> Result<[URL], FileCopyService.FileCopyError> {
+    SecurityScopedHelper.access(url) {
+      do {
+        let contents = try FileManager.default.contentsOfDirectory(
+          at: url,
+          includingPropertiesForKeys: nil,
+          options: [.skipsHiddenFiles]
+        )
+        return .success(contents)
+      } catch {
+        return .failure(.insufficientPermissions)
+      }
+    }
+  }
+  
+  
+  func secureBaseFolder(_ url: URL) async -> Result<URL, FileCopyService.FileCopyError> {
+    SecurityScopedHelper.createSecureBookmark(for: url)
+  }
+  
+  // TODO: Make use of listCOntents
+  func countPhotos(url: URL) async -> Result<Int, FileCopyService.FileCopyError> {
+    do {
+      let contents = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [])
+      let photoCount = contents.filter { $0.pathExtension.lowercased() == "jpg" || $0.pathExtension.lowercased() == "png" }.count
+      return .success(photoCount)
+    } catch {
+      return .failure(.unknownError("Count Photos Error"))
+    }
+  }
   
 }
 
 extension FileManagerClient: DependencyKey {
-  
-  static let liveValue = FileManagerClient(
-    createDirectory: { baseURL, name in
-      SecurityScopedHelper.access(baseURL) {
-        let newDirURL = baseURL.appendingPathComponent(name)
-        do {
-          try FileManager.default.createDirectory(
-            at: newDirURL,
-            withIntermediateDirectories: true,
-            attributes: nil
-          )
-          return SecurityScopedHelper.createSecureBookmark(for: newDirURL)
-        } catch {
-          return .failure(.customerDirectoryCreationFailed)
-        }
-      }
-    },
-    
-    getDirectory: { baseURL, name in
-      let dirURL = baseURL.appendingPathComponent(name)
-      if FileManager.default.fileExists(atPath: dirURL.path) {
-        return SecurityScopedHelper.createSecureBookmark(for: dirURL)
-      } else {
-        return .failure(.directoryNotFound(dirURL.lastPathComponent))
-      }
-    },
-    
-    listContents: { url in
-      SecurityScopedHelper.access(url) {
-        do {
-          let contents = try FileManager.default.contentsOfDirectory(
-            at: url,
-            includingPropertiesForKeys: nil,
-            options: [.skipsHiddenFiles]
-          )
-          return .success(contents)
-        } catch {
-          return .failure(.insufficientPermissions)
-        }
-      }
-    },
-    
-    secureBaseFolder: { url in
-      SecurityScopedHelper.createSecureBookmark(for: url)
-    },
-    
-    countPhotos: { url in
-      do {
-        let contents = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [])
-        let photoCount = contents.filter { $0.pathExtension.lowercased() == "jpg" || $0.pathExtension.lowercased() == "png" }.count
-        return .success(photoCount)
-      } catch {
-        return .failure(.unknownError("Count Photos Error"))
-      }
-    }
-  )
+  static let liveValue = FileManagerClient()
 }
 
 extension DependencyValues {
@@ -76,10 +71,3 @@ extension DependencyValues {
   }
 }
 
-//extension String {
-//  func sanitizedCustomerInput() -> String {
-//    return self.trimmingCharacters(in: .whitespaces)
-//      .components(separatedBy: .init(charactersIn: "/\\:*?\"<>|"))
-//      .joined()
-//  }
-//}
